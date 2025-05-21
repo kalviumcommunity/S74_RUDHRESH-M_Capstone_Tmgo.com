d008899ead4986096318f1d40a49c46055eb1934
RUDHRESH M <rudhreshm191106@gmail.com>
2025-05-21 13:27:09 +0530

    Added POST endpoint for adding new places

--- backend/controllers/placesController.js
+++ backend/controllers/placesController.js
@@ -1,5 +1,6 @@
 const Place = require('../models/Place');
 
+// GET All Places (Already exists)
 const getPlaces = async (req, res) => {
   try {
     const places = await Place.find();
@@ -9,4 +10,17 @@ const getPlaces = async (req, res) => {
   }
 };
 
-module.exports = { getPlaces };
+// POST - Add New Place
+const createPlace = async (req, res) => {
+  const { name, description, location } = req.body;
+
+  try {
+    const newPlace = new Place({ name, description, location });
+    await newPlace.save();
+    res.status(201).json(newPlace);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+module.exports = { getPlaces, createPlace };
